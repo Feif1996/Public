7c33282faa9a991d28d54cc97f39b9af12213217
Feif1996 <feif19423@gmail.com>
2024-06-29 03:59:17 +0800

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace CplexModelProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, CPLEX Model!");
+
+            // 在這裡實現你的 CPLEX 模型邏輯
+        }
+    }
+}
